2a442d5cd1994bd3f2894aa6ece5c351b8d36daa
Raghav <vahgarkhurana@gmail.com>
2016-07-15 13:52:49 +0530

    Static Settings

--- source/settings.py
+++ source/settings.py
@@ -110,10 +110,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_ROOT = 'staticfiles'
+# STATIC_ROOT = 'staticfiles'
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
+
+STATIC_ROOT = '/~/toHost/static'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
-
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
